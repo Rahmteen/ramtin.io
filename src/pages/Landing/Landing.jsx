eb00e3146dcec0c0468033df0c722eea5cbcf3ea
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2021-05-25 01:21:52 -0700

    updated styling landing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useContext, Fragment } from 'react';
-import { StateContext } from '../../providers/StateProvider';
 import { Link } from 'react-router-dom';
 import { Container, Col, Row, Image, Button } from 'react-bootstrap';
 import rk_logo from '../../../assets/images/RKlogo.png';
@@ -10,27 +9,23 @@ const Landing = () => {
 			<Container className="landing-container " fluid>
 				<Row className="landing-row-main text-center">
 					<Col lg={12} md={8} className="text-left">
-						<Link to='/works'>
+						<Link to="/works">
 							{' '}
 							<Image src={rk_logo} className="brand"></Image>
 						</Link>
 					</Col>
 					<Col lg={12} md={8} className=" my-auto text-mono text-right font-tertiary">
-						
-							<span className="text-mono"></span>
-							My name is Ramtin Khoee.
-						
-							<br />
-							<span className="text-mono">
-								I solve problems <br /> and create projects.
-							</span>
-					
-							<br />
-							<Link to="/works">
-								<span className="take-a-look text-mono pt-5">Take a look...</span>
-							</Link>
-							{/* <Typing.Backspace count={20} /> */}
-						
+						<span className="text-mono"></span>
+						My name is Ramtin Khoee.
+						<br />
+						<span className="text-mono">
+							I solve problems <br /> and create projects.
+						</span>
+						<br />
+						<br />
+						<Link to="/works">
+							<span className="take-a-look text-mono pt-5"><Button variant="outline-success">Take A Look</Button></span>
+						</Link>
 					</Col>
 				</Row>
 			</Container>
